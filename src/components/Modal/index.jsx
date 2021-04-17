10ff038be8806911f3749a90f8099faa7b104d19
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2021-04-17 12:23:26 -0300

    Update Modal component

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,33 +1,11 @@
-import { Component } from 'react';
 import ReactModal from 'react-modal';
 
-class Modal extends Component {
-  constructor(props) {
-    super(props);
-
-    const { isOpen } = this.props;
-    this.state = {
-      modalStatus: isOpen
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    const { isOpen } = this.props;
-
-    if (prevProps.isOpen !== isOpen) {
-      this.setState({ modalStatus: isOpen })
-    }
-  }
-
-  render() {
-    const { children, setIsOpen } = this.props;
-    const { modalStatus } = this.state;
-
+function Modal({ isOpen, setIsOpen, children}) {
     return (
       <ReactModal
         shouldCloseOnOverlayClick={!false}
         onRequestClose={setIsOpen}
-        isOpen={modalStatus}
+        isOpen={isOpen}
         ariaHideApp={false}
         style={{
           content: {
@@ -50,8 +28,7 @@ class Modal extends Component {
       >
         {children}
       </ReactModal>
-    );
-  }
-};
+    )
+}
 
 export default Modal;
